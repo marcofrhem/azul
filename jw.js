369e8a818f36ce981d3bde04835de9a830466000
marco <marco@windos.lan>
2021-05-29 15:37:44 -0300

    First commit

---
+++ jw.js
@@ -0,0 +1,11 @@
+--2021-05-29 14:23:03--  https://luiz-lp.github.io/crpiframeplayer/
+Resolvendo luiz-lp.github.io (luiz-lp.github.io)... 185.199.111.153, 185.199.110.153, 185.199.109.153, ...
+Conectando-se a luiz-lp.github.io (luiz-lp.github.io)|185.199.111.153|:443... conectado.
+A requisição HTTP foi enviada, aguardando resposta... 200 OK
+Tamanho: 3377 (3,3K) [text/html]
+Salvando em: “index.html”
+
+     0K ...                                                   100% 11,2M=0s
+
+2021-05-29 14:23:04 (11,2 MB/s) - “index.html” salvo [3377/3377]
+
